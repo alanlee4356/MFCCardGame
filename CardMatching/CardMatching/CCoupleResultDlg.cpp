26accdebd5072f42e1f7b788a85788de8ddbf659
alanlee4356 <alanlee4356@gmail.com>
2024-11-21 06:55:55 +0900

    pch 변수 형식 변경, 결과창 1인용 2인용 디자인, 및 점수 표시 기능 구현

---
+++ CardMatching/CardMatching/CCoupleResultDlg.cpp
@@ -0,0 +1,136 @@
+﻿// CCoupleResultDlg.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "CardMatching.h"
+#include "afxdialogex.h"
+#include "CCoupleResultDlg.h"
+#include "resource.h"
+
+
+// CCoupleResultDlg 대화 상자
+
+IMPLEMENT_DYNAMIC(CCoupleResultDlg, CDialogEx)
+
+CCoupleResultDlg::CCoupleResultDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_COUPLE_RESULT_DIALOG, pParent)
+{/*
+	CString str_Score;
+	CString str_Time;
+	CString str_FinalScore;
+	CString str_Score2;
+	CString str_Time2;
+	CString str_FinalScore2;
+	CString str_Winner;
+	
+	//전역변수에 있는 score, time 값을 바탕으로 값을 계산 후 표시
+	m_nFinalScore = m_nScore + m_nTime;
+	str_Score.Format(_T("%d"), m_nScore);
+	str_Time.Format(_T("%d"), m_nTime);
+	str_FinalScore.Format(_T("%d"), m_nFinalScore);
+	m_nFinalScore2 = m_nScore2 + m_nTime2;
+	str_Score2.Format(_T("%d"), m_nScore2);
+	str_Time2.Format(_T("%d"), m_nTime2);
+	str_FinalScore2.Format(_T("%d"), m_nFinalScore2);
+	
+	if (m_nFinalScore > m_nFinalScore2) {
+		str_Winner.Format(_T("1P Win!"))
+	}
+	else if (m_nFinalScore == m_nFinalScore2) {
+		str_Winner.Format(_T("Draw"))
+	}
+	else {
+		str_Winner.Format(_T("2P Win!"))
+	}
+	
+	m_score.SetWindowTextW(_T("0"));
+	m_finalScore.SetWindowTextW(_T("0"));
+	m_time.SetWindowTextW(_T("0"));
+	m_score2.SetWindowTextW(_T("0"));
+	m_finalScore2.SetWindowTextW(_T("0"));
+	m_time2.SetWindowTextW(_T("0"));
+	m_winner.SetWindowTextW(_T("0"));
+	*/
+}
+
+CCoupleResultDlg::~CCoupleResultDlg()
+{
+}
+
+void CCoupleResultDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	/*
+	DDX_Control(pDX, IDC_STATIC_SCORE1, m_score);
+	//DDX_Control(pDX, IDC_STATIC_SCORE2, m_score2);
+	DDX_Control(pDX, IDC_STATIC_TIME1, m_time);
+	//  DDX_Control(pDX, IDC_STATIC_TIME2, m_time2);
+	//  DDX_Control(pDX, IDC_STATIC_WINNER, m_winner);
+	DDX_Control(pDX, IDC_STATIC_FINALSCORE1, m_finalScore);
+	//  DDX_Control(pDX, IDC_STATIC_FINALSCORE2, m_finalScore2);
+	//DDX_Control(pDX, IDC_STATIC_WINNER, m_winner);
+	//DDX_Control(pDX, IDC_STATIC_WINNER1, m_winner);
+	DDX_Control(pDX, IDC_STATIC_WINNER, m_winner);
+	//  DDX_Control(pDX, IDC_STATIC_ASDF, m_asdf);
+	*/
+	DDX_Control(pDX, IDC_STATIC_TIME, m_time);
+	DDX_Control(pDX, IDC_STATIC_TIME2, m_time2);
+	DDX_Control(pDX, IDC_STATIC_SCORE3, m_score2);
+	DDX_Control(pDX, IDC_STATIC_SCORE, m_score);
+	DDX_Control(pDX, IDC_STATIC_FINALSCORE2, m_finalScore2);
+	DDX_Control(pDX, IDC_STATIC_FINALSCORE, m_finalScore);
+}
+
+
+BEGIN_MESSAGE_MAP(CCoupleResultDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CCoupleResultDlg 메시지 처리기
+
+
+BOOL CCoupleResultDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// TODO:  여기에 추가 초기화 작업을 추가합니다.
+	CString str_Score;
+	CString str_Time;
+	CString str_FinalScore;
+	CString str_Score2;
+	CString str_Time2;
+	CString str_FinalScore2;
+	CString str_Winner;
+
+	//전역변수에 있는 score, time 값을 바탕으로 값을 계산 후 표시
+	m_nFinalScore = m_nScore + m_nTime;
+	str_Score.Format(_T("%d"), m_nScore);
+	str_Time.Format(_T("%d"), m_nTime);
+	str_FinalScore.Format(_T("%d"), m_nFinalScore);
+	m_nFinalScore2 = m_nScore2 + m_nTime2;
+	str_Score2.Format(_T("%d"), m_nScore2);
+	str_Time2.Format(_T("%d"), m_nTime2);
+	str_FinalScore2.Format(_T("%d"), m_nFinalScore2);
+	/*
+	if (m_nFinalScore > m_nFinalScore2) {
+		str_Winner.Format(_T("1P Win!"))
+	}
+	else if (m_nFinalScore == m_nFinalScore2) {
+		str_Winner.Format(_T("Draw"))
+	}
+	else {
+		str_Winner.Format(_T("2P Win!"))
+	}
+	*/
+	m_score.SetWindowTextW(str_Score);
+	m_finalScore.SetWindowTextW(str_FinalScore);
+	m_time.SetWindowTextW(str_Time);
+	m_score2.SetWindowTextW(str_Score2);
+	m_finalScore2.SetWindowTextW(str_FinalScore2);
+	m_time2.SetWindowTextW(str_Time2);
+	//m_winner.SetWindowTextW(_T("0"));
+	
+
+	return TRUE;  // return TRUE unless you set the focus to a control
+				  // 예외: OCX 속성 페이지는 FALSE를 반환해야 합니다.
+}
---
+++ CardMatching/CardMatching/CCoupleResultDlg.h
@@ -0,0 +1,45 @@
+﻿#pragma once
+#include "afxdialogex.h"
+
+
+// CCoupleResultDlg 대화 상자
+
+class CCoupleResultDlg : public CDialogEx
+{
+	DECLARE_DYNAMIC(CCoupleResultDlg)
+
+public:
+	CCoupleResultDlg(CWnd* pParent = nullptr);   // 표준 생성자입니다.
+	virtual ~CCoupleResultDlg();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_COUPLE_RESULT_DIALOG };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+public:
+//	CStatic m_finalScore;
+//	CStatic m_finalScore2;
+//	CStatic m_score;
+//	CStatic m_score2;
+//	CStatic m_time;
+//	CStatic m_time2;
+//	CStatic m_winner;
+//	CStatic m_finalScore;
+//	CStatic m_finalScore2;
+//	CStatic m_winner;
+//	CStatic m_winner;
+//	CStatic m_winner;
+//	CStatic m_asdf;
+	CStatic m_time;
+	CStatic m_time2;
+	CStatic m_score2;
+	CStatic m_score;
+	CStatic m_finalScore2;
+	CStatic m_finalScore;
+	virtual BOOL OnInitDialog();
+};
